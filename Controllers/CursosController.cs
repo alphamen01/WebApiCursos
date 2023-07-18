d4f8357d740aa0234d7a51aefc08dcaa347d35f6
alphamen01 <lesg.2018@hotmail.com>
2023-07-17 20:30:25 -0500

    Creacion de proyecto Web API y creacion de controlador API CursosController

---
+++ Controllers/CursosController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiCursos.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CursosController : ControllerBase
+	{
+		// GET: api/<CursosController>
+		[HttpGet]	
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<CursosController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<CursosController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<CursosController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<CursosController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
