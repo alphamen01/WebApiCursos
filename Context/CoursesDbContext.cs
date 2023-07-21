1da22281a30a7e8d881270ef8cc86e773a3473c3
alphamen01 <lesg.2018@hotmail.com>
2023-07-21 09:02:24 -0500

    creacion de la clase dbcontext

---
+++ Context/CoursesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCursos.Models;
+
+namespace WebApiCursos.Context
+{
+	public class CoursesDbContext: DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=PROYCANVSIN19;Initial Catalog=Courses;User ID=sa;Password=Lesg2022;");
+		}
+
+		public DbSet<Course> Courses { get; set; }
+    }
+}
--- Providers/SqlServerCourseProvider.cs
+++ Providers/SqlServerCourseProvider.cs
@@ -1,6 +1,35 @@
-﻿namespace WebApiCursos.Providers
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApiCursos.Interfaces;
+using WebApiCursos.Models;
+
+namespace WebApiCursos.Providers
 {
-	public class SqlServerCourseProvider
+	public class SqlServerCourseProvider : ICoursesProvider
 	{
+		public Task<(bool IsSuccess, int? Id)> AddAsync(Course course)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<ICollection<Course>> GetAllAsync()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<Course> GetAsync(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<ICollection<Course>> SearchAsync(string search)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<bool> UpdateAsync(int id, Course course)
+		{
+			throw new System.NotImplementedException();
+		}
 	}
 }
