a079cb1c0f5a9294b62df26d825ece10c7d9f45a
alphamen01 <lesg.2018@hotmail.com>
2023-07-21 17:31:09 -0500

    implementando servicio de base de datos relacional RDS-AWS/SQLServer

--- Context/CoursesDbContext.cs
+++ Context/CoursesDbContext.cs
@@ -7,7 +7,7 @@ namespace WebApiCursos.Context
 	{
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer(@"Data Source=PROYCANVSIN19;Initial Catalog=Courses;User ID=sa;Password=Lesg2022;");
+			optionsBuilder.UseSqlServer(@"Data Source=sqlserver.cttk8gyj8bcf.us-east-1.rds.amazonaws.com;Initial Catalog=Courses;User ID=sa;Password=Lesg2022;");
 		}
 
 		public DbSet<Course> Courses { get; set; }
